4e727148af54bda1280386edf21a9b6b4575967d
Ilariya Solona <lkroft451@gmail.com>
2025-12-12 23:09:45 +0200

    testing after review: true

--- app/Services/Users/Routes/routes.php
+++ app/Services/Users/Routes/routes.php
@@ -4,25 +4,15 @@ use Illuminate\Support\Facades\Route;
 use App\Services\Users\Controllers\UserController;
 
 Route::prefix('users')->group(function () {
-    // Реєстрація та верифікація
     Route::prefix('registration')->group(function () {
         Route::post('/', [UserController::class, 'registration']);
-
-        // Маршрут для повторної відправки листа
         Route::post('/email/verification-notification', [UserController::class, 'newlink'])
-            ->middleware(['throttle:6,1'])
-            ->name('verification.send');
-
-        // Головний маршрут верифікації (додано middleware 'signed')
+            ->middleware(['throttle:6,1']);
         Route::get('/email/verify/{id}/{hash}', [UserController::class, 'verify'])
-            ->middleware(['signed', 'throttle:6,1'])
-            ->name('verification.verify');
+            ->middleware(['signed', 'throttle:6,1']);
     });
 
-    // Авторизація (Вхід) - змінено на POST
     Route::post('/login', [UserController::class, 'login']);
-
-    // Група маршрутів, що вимагають авторизації (JWT)
     Route::middleware('auth:api')->group(function () {
         Route::post('/logout', [UserController::class, 'destroy']);
         Route::get('/me', [UserController::class, 'me']);
